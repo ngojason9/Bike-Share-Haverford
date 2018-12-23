fb70a9120da02a3322fcc6216b7f715ae05875e0
Jason Ngo <ngojason9@gmail.com>
2018-12-23 14:16:52 -0500

    Update log in forms

--- app/routes.py
+++ app/routes.py
@@ -3,12 +3,12 @@ from app import app
 from app.forms import LoginForm
 
 @app.route("/")
-@app.route("/home")
-def home():
+@app.route("/index")
+def index():
     user = {'username': 'Jason'}
-    return render_template("home.html", user = user)
+    return render_template("index.html", user = user)
 
 @app.route("/login")
 def login():
     form = LoginForm()
-    return render_template('login.html', form=form)
+    return render_template('login.html', title='Sign In', form=form)
