f91afeb61438a75f3679a1ea762cf01ba08402d5
Jason Ngo <ngojason9@gmail.com>
2019-01-09 11:51:34 -0500

    Create countdown clock

--- app/static/timer.js
+++ app/static/timer.js
@@ -1,14 +1,16 @@
 $(document).ready(function() {
     function startTimer(duration, display) {
-        var timer = duration, minutes, seconds;
+        var timer = duration, hours, minutes, seconds;
         var refresh = setInterval(function () {
-            minutes = parseInt(timer / 60, 10)
+            hours = parseInt(timer / 3600, 10)
+            minutes = parseInt( (timer - hours * 3600 ) / 60, 10)
             seconds = parseInt(timer % 60, 10);
 
+            hours = "0" + hours;
             minutes = minutes < 10 ? "0" + minutes : minutes;
             seconds = seconds < 10 ? "0" + seconds : seconds;
 
-            var output = minutes + " : " + seconds;
+            var output = hours + " : " + minutes + " : " + seconds;
             display.text(output);
             $("title").html(output + " - TimerTimer");
 
