e78aba2ccbec0111e99ca80e8d3afe2542c713f6
Jason Ngo <ngojason9@gmail.com>
2019-01-09 11:14:41 -0500

    Initial commit

---
+++ app/static/timer.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+    function startTimer(duration, display) {
+        var timer = duration, minutes, seconds;
+        var refresh = setInterval(function () {
+            minutes = parseInt(timer / 60, 10)
+            seconds = parseInt(timer % 60, 10);
+
+            minutes = minutes < 10 ? "0" + minutes : minutes;
+            seconds = seconds < 10 ? "0" + seconds : seconds;
+
+            var output = minutes + " : " + seconds;
+            display.text(output);
+            $("title").html(output + " - TimerTimer");
+
+            if (--timer < 0) {
+                display.text("Time's Up!");
+                clearInterval(refresh);  // exit refresh loop
+            }
+        }, 1000);
+
+    }
+
+    // start timer
+    jQuery(function ($) {
+        var display = $('#time');
+        startTimer(Seconds, display);
+    });
+
+})
