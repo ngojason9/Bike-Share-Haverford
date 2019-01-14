35ffb5a853d6a4de1cc286171cdefa787c6c5cec
Jason Ngo <ngojason9@gmail.com>
2019-01-14 13:29:48 -0500

    initial deployment

--- config.py
+++ config.py
@@ -8,9 +8,11 @@ class Config(object):
         'DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')        
+
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['ngojason9@gmail.com']
+    ADMINS = ['jdagod@yahoo.com']
